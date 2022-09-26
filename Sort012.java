1c4dfa7501cdf2186ad324b77b3f34d17ef38a16
CozevaQA <releaseteam-ccu@cozeva.com>
2022-09-27 01:34:24 +0530

    Sort012

--- Sort012.java
+++ Sort012.java
@@ -1,3 +1,51 @@
+import java.util.Scanner;
+
 public class Sort012 {
-    
+    /**
+     * @param args
+     */
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i] = sc.nextInt();
+            }
+
+            sol(arr);
+            for(int i=0;i<n;i++){
+                System.out.print(arr[i]);
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void sol(int arr[]){
+        int n = arr.length;
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
 }
